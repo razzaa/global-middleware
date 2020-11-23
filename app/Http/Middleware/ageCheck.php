21d07663f7d11c84fd3a9391ed7994ef3320a12d
alibulc <razabulc@gmail.com>
2020-11-23 11:13:43 +0500

    Intial Commit

---
+++ app/Http/Middleware/ageCheck.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ageCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->age && $request->age<18){
+            return redirect('noaccess');
+        }
+        return $next($request);
+    }
+}
---
+++ resources/views/noaccess.blade.php
@@ -0,0 +1,1 @@
+<h1>UnAuthorized Access!!!</h1>
